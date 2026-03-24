96ebe32114a991bb45cc1595c8a7d51927e6c6c6
Nitesh Negi <nitesh_negi@outlook.com>
2026-03-24 12:02:32 +0530

    Dark mode initiated by ShadCN

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Provider from "./provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,10 +20,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en"
+      lang="en" suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Provider>
+          {children}
+        </Provider>
+        </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
+"use client";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
 
 export default function Home() {
+  const {setTheme} = useTheme()
   return (
     <div>
       <h2>Great People</h2>
+      <Button>Click me</Button>
+      <Button onClick={() => setTheme("light")}>Light</Button>
+      <Button onClick={() => setTheme("dark")}>Dark</Button>
+
     </div>
   );
 }
