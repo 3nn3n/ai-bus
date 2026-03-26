3a6da4c248326fe57c7813d0cc5a57255db2101d
Nitesh Negi <nitesh_negi@outlook.com>
2026-03-26 10:39:36 +0530

    10. Multi_ai response and database update

--- app/_components/AiMultiModelTab.jsx
+++ app/_components/AiMultiModelTab.jsx
@@ -12,7 +12,7 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 import { Switch } from '@/components/ui/switch'
-import { MessageSquare } from 'lucide-react'
+import { Loader, MessageSquare } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { Lock } from 'lucide-react'
 import { useContext } from 'react'
@@ -20,11 +20,24 @@ import { AiSelectedModelContext } from '@/context/AiSelectedModel'
 import { doc, updateDoc } from 'firebase/firestore'
 import { db } from '@/config/FirebaseConfig'
 import { useUser } from '@clerk/nextjs'
+import { useEffect } from 'react'
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 
 function AiMultiModelTab() {
-  const {user} = useUser();
+  const { user } = useUser();
   const [aiModelList, setAiModelList] = useState(AiModelList);
-  const {aiSelectedModel, setAiSelectedModel} = useContext(AiSelectedModelContext);
+  const { aiSelectedModel, setAiSelectedModel, messages, setMessages } = useContext(AiSelectedModelContext);
+
+  // Add this useEffect to sync UI with context loaded from Firebase
+  useEffect(() => {
+    setAiModelList((prevList) =>
+      prevList.map((item) => ({
+        ...item,
+        enable: aiSelectedModel[item.model]?.enable ?? item.enable,
+      }))
+    );
+  }, [aiSelectedModel]);
 
   const onSelectValueChange = async (parentModel, value) => {
     const updatedModel = {
@@ -44,58 +57,83 @@ function AiMultiModelTab() {
 
 
 
-  const toggleModelEnable = (model, value) => {
+  const toggleModelEnable = async (model, value) => {
     setAiModelList((prevList) =>
       prevList.map((item) =>
         item.model === model ? { ...item, enable: value } : item
       )
     );
-  }
+
+    const updatedModel = {
+      ...aiSelectedModel,
+      [model]: {
+        ...(aiSelectedModel?.[model] ?? {}),
+        enable: value,
+      }
+    };
+    setAiSelectedModel(updatedModel);
+
+    const docRef = doc(db, "users", user?.primaryEmailAddress?.emailAddress);
+    await updateDoc(docRef, { selectedModelPref: updatedModel });
+  };
+
+  console.log("Selected Models:", aiSelectedModel);
 
   return (
     <div className='flex flex-1 h-[70vh] border-b'>
       {aiModelList.map((model, index) => (
         <div key={index} className={`flex flex-col h-full border-r
           ${model.enable ? 'flex-1 min-w-[300px]' : 'w-[100px] flex-none'}`}>
-        
-        <div className='flex w-full h-[70px] shrink-0 items-center justify-between border-b p-4'>
-          <div className='flex items-center gap-4'>
-            <Image src={model.icon} alt={model.model} width={20} height={20} />
-           {model.enable && (
-            <Select value={aiSelectedModel[model.model]?.modelId} onValueChange={(value) => onSelectValueChange(model.model, value)} disabled={model.premium}>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Select a model" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup className="px-3">
-                  <SelectLabel className={"text-sm text-gray-400"}>Free</SelectLabel>
-                {model.subModel.map((subModel, subIndex) => subModel.premium == false && (
-                  <SelectItem key={subIndex} value={subModel.id}>{subModel.name}</SelectItem>
-                ))}
-                </SelectGroup>
-                <SelectGroup className="px-3">
-                  <SelectLabel className={"text-sm text-gray-400"}>Premium</SelectLabel>
-                {model.subModel.map((subModel, subIndex) => subModel.premium == true && (
-                  <SelectItem key={subIndex} value={subModel.id} disabled={subModel.premium}>{subModel.name}{subModel.premium && <Lock className='h-2 w-2 ml-2'/>}</SelectItem>
-                ))}
-                </SelectGroup>
-              </SelectContent>
-            </Select>)}
-          </div>
-          <div>
-            {model.enable ?<Switch checked={model.enable}
-              onCheckedChange={(v) => toggleModelEnable(model.model, v)}
-            /> : <MessageSquare onClick={() => toggleModelEnable(model.model, true)}
-              
-            />}
+
+          <div className='flex w-full h-[70px] shrink-0 items-center justify-between border-b p-4'>
+            <div className='flex items-center gap-4'>
+              <Image src={model.icon} alt={model.model} width={20} height={20} />
+              {model.enable && (
+                <Select value={aiSelectedModel[model.model]?.modelId} onValueChange={(value) => onSelectValueChange(model.model, value)} disabled={model.premium}>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Select a model" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup className="px-3">
+                      <SelectLabel className={"text-sm text-gray-400"}>Free</SelectLabel>
+                      {model.subModel.map((subModel, subIndex) => subModel.premium == false && (
+                        <SelectItem key={subIndex} value={subModel.id}>{subModel.name}</SelectItem>
+                      ))}
+                    </SelectGroup>
+                    <SelectGroup className="px-3">
+                      <SelectLabel className={"text-sm text-gray-400"}>Premium</SelectLabel>
+                      {model.subModel.map((subModel, subIndex) => subModel.premium == true && (
+                        <SelectItem key={subIndex} value={subModel.id} disabled={subModel.premium}>{subModel.name}{subModel.premium && <Lock className='h-2 w-2 ml-2' />}</SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>)}
             </div>
-        </div>
-       {model.premium && model.enable  && <div className='flex flex-1 items-center justify-center'>
-        <Button> <Lock/>Upgrade to Unlock</Button>
-      </div>}
+            <div>
+              {model.enable ? <Switch checked={model.enable}
+                onCheckedChange={(v) => toggleModelEnable(model.model, v)}
+              /> : <MessageSquare onClick={() => toggleModelEnable(model.model, true)}
+
+              />}
+            </div>
+          </div>
+          {model.premium && model.enable && <div className='flex flex-1 items-center justify-center'>
+            <Button> <Lock />Upgrade to Unlock</Button>
+          </div>}
+          {model.enable && <div className='flex-1 p-4 overflow-auto'>
+            <div className='p-4 flex flex-col flex-1 space-y-2'>
+              {messages[model.model]?.map((m, i) => (
+                <div key={i} className={`p-2 rounded-md ${m.role === "user" ? "bg-orange-100 text-orange-500" : "bg-gray-200 text-gray-500"}`}>
+                  {m.role === "assistant" && <span className='text-sm text-gray-400'>{model.model}</span>}
+                  <div className='flex items-center gap-3'> {m.content == "Thinking..." && <><Loader className='animate-spin' /><span>Thinking...</span></>}</div>
+                  {m.content !== "Thinking..." && 
+                  <Markdown remarkPlugins={[remarkGfm]}>{m.content}</Markdown>}
+                </div>
+              ))}
+            </div>
+          </div>}
         </div>
       ))}
-      
 
     </div>
   )
