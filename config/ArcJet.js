692d6a1938fbae5e01d9447847dfaf1703b2349d
Nitesh Negi <nitesh_negi@outlook.com>
2026-03-27 11:17:02 +0530

    changed interval

--- config/ArcJet.js
+++ config/ArcJet.js
@@ -9,7 +9,7 @@ export const aj = arcjet({
       mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
       characteristics: ["userId"], // track requests by a custom user ID
       refillRate: 5, // refill 5 tokens per interval
-      interval: 5, // refill every 5 seconds (in seconds)
+      interval: 8460, // refill every 8460 seconds (in seconds)
       capacity:5, // bucket maximum capacity of 5 tokens
     }),
   ],
